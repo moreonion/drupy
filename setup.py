07eb0e29b70e33d884c5136ee79b67aaac4fbfd2
Roman Zimmermann <roman@more-onion.com>
2017-01-16 15:12:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 from setuptools import setup
 
 setup(
-  name='drupy',
-  version='0.1',
-  description='Python based deployment tool for drupal',
-  long_description='Fast, multisite capable drupal deployment tool',
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Environment :: Console',
-    'Intended Audience :: Developers',
-    'Intended Audience :: System Administrators',
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-    'Topic :: System :: Software Distribution',
-  ],
-  keywords='drupal drupy build',
+    name='drupy',
+    version='0.1',
+    description='Python based deployment tool for drupal',
+    long_description='Fast, multisite capable drupal deployment tool',
+    author='Roman Zimmermann',
+    author_email='torotil@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Topic :: System :: Software Distribution',
+    ],
+    keywords='drupal drupy build',
 
-  url = 'https://github.com/moreonion/drupy',
+    url='https://github.com/moreonion/drupy',
+    download_url='https://github.com/moreonion/drupy/archive/v0.1.tar.gz',
 
-  packages=['drupy'],
-  entry_points = {
-    'console_scripts': ['drupy = drupy.runner:main'],
-  },
-  include_package_data = True,
+    packages=['drupy'],
+    entry_points={
+        'console_scripts': ['drupy = drupy.runner:main'],
+    },
+    include_package_data=True,
 )
