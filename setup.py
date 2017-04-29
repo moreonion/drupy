583eafe20d755480aa733897d062d9a8d6766db3
Roman Zimmermann <roman@more-onion.com>
2017-04-29 15:56:08 +0200

    Version v0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='drupy',
-    version='0.2',
+    version='0.3',
     description='Python based deployment tool for drupal',
     long_description='Fast, multisite capable drupal deployment tool',
     author='Roman Zimmermann',
@@ -18,7 +18,7 @@ setup(
     keywords='drupal drupy build',
 
     url='https://github.com/moreonion/drupy',
-    download_url='https://github.com/moreonion/drupy/archive/v0.2.tar.gz',
+    download_url='https://github.com/moreonion/drupy/archive/v0.3.tar.gz',
 
     packages=['drupy'],
     entry_points={
